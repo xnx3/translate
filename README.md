b0981d5037861ca760c8f4eea5429435f885b99c
xnx3 <mail@xnx3.com>
2022-04-04 15:51:21 +0800

    import

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 ## 介绍
 国际化，网页自动翻译，同谷歌浏览器自动翻译的效果，适用于网站
 
-## 托管
-托管于 http://res.zvo.cn/translate/
 
 ## 使用
 直接在要显示的位置引入js即可
@@ -14,14 +12,14 @@
 ````
 
 ## 美化 切换语言 的位置
-可使用css定义 #translate{} 来定义切换语言的显示。如：
+可使用css来控制切换语言选择的显示位置及美观。如：
 
 ````
 <style>
-#translate{
+.translateSelectLanguage{
 	position: absolute;
-	top:10px;
-	right:10px;
+	top:100px;
+	right:100px;
 }
 </style>
 ````
--- translate.js
+++ translate.js
@@ -8,6 +8,10 @@ var translate = {
 	 */
 	version:1.0,
 	/*
+	 * 翻译的对象，也就是 new google.translate.TranslateElement(...)
+	 */
+	translate:null,
+	/*
 	 * 资源文件url的路径
  	 */
 	resourcesUrl:'//res.zvo.cn.obs.cn-north-4.myhuaweicloud.com/translate',
@@ -27,15 +31,15 @@ var translate = {
 	 * google翻译执行的
 	 */
 	googleTranslateElementInit:function(){
-		var trans = new google.translate.TranslateElement(
+		translate.translate = new google.translate.TranslateElement(
 			{
 				//这参数没用，请忽略
-				//pageLanguage: 'zh-CN',
+				pageLanguage: 'zh-CN',
 				//一共80种语言选择，这个是你需要翻译的语言，比如你只需要翻译成越南和英语，这里就只写en,vi
 				//includedLanguages: 'de,hi,lt,hr,lv,ht,hu,zh-CN,hy,uk,mg,id,ur,mk,ml,mn,af,mr,uz,ms,el,mt,is,it,my,es,et,eu,ar,pt-PT,ja,ne,az,fa,ro,nl,en-GB,no,be,fi,ru,bg,fr,bs,sd,se,si,sk,sl,ga,sn,so,gd,ca,sq,sr,kk,st,km,kn,sv,ko,sw,gl,zh-TW,pt-BR,co,ta,gu,ky,cs,pa,te,tg,th,la,cy,pl,da,tr',
 	            includedLanguages: 'zh-CN,zh-TW,en',
 				//选择语言的样式，这个是面板，还有下拉框的样式，具体的记不到了，找不到api~~  
-				layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+				layout: 0,
 				//自动显示翻译横幅，就是翻译后顶部出现的那个，有点丑，设置这个属性不起作用的话，请看文章底部的其他方法
 				//autoDisplay: false, 
 				//disableAutoTranslation:false,
@@ -46,7 +50,7 @@ var translate = {
 	},
 	
 	/**
-	 * 初始化
+	 * 初始化，如加载js、css资源
 	 */
 	init:function(){
 		/****** 先判断当前协议，定义资源路径  ******/
@@ -67,15 +71,19 @@ var translate = {
 		var head0 = document.getElementsByTagName('head')[0];
 		var script = document.createElement("script");  //创建一个script标签
 		script.type = "text/javascript";
-		script.async = true;
+		//script.async = true;
 		script.src = this.resourcesUrl+'/js/element.js';
 		head0.appendChild(script);
-	}
+	},
+	/**
+	 * 执行翻译操作
+	 */
+	trans:function(){
+		this.googleTranslateElementInit();
+	},
 }
 
 
 try{
-	
-	
 	translate.init();
 }catch(e){ console.log(e); }

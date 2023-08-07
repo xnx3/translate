3a00d7343e3beb26c2627c642a7a7b15db55e2cb
xnx3 <mail@xnx3.com>
2023-08-07 18:46:58 +0800

    完善 translate.service.deploy

--- translate.service/deploy/src/main/java/cn/ServletInitializer.java
+++ translate.service/deploy/src/main/java/cn/ServletInitializer.java
@@ -1,11 +1,9 @@
-package com;
+package cn;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import com.xnx3.j2ee.util.ConsoleUtil;
 
 /**
  * 放到Tomcat中时用
@@ -16,14 +14,6 @@ import com.xnx3.j2ee.util.ConsoleUtil;
 public class ServletInitializer extends SpringBootServletInitializer {
     
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    	ConsoleUtil.debug = true;
-    	ConsoleUtil.info = true;
-    	ConsoleUtil.error = true;
-    	com.xnx3.j2ee.Global.isJarRun = false;	//自行放到tomcat中运行
-    	application.allowCircularReferences(true);
-    	
-    	Application.startFinish();//当项目启动完毕后，控制台打印启动成功的说明指引
-    	
     	return application.sources(Application.class);
     }
 }

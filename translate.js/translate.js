d071de73b197e1b4b78a686948c893b9c020a568
xnx3 <mail@xnx3.com>
2023-08-08 14:15:55 +0800

    更新版本号

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -9,7 +9,7 @@ var translate = {
 	/*
 	 * 当前的版本
 	 */
-	version:'2.5.1.20230802',
+	version:'2.5.2.20230808',
 	useVersion:'v1',	//当前使用的版本，默认使用v1. 可使用 setUseVersion2(); //来设置使用v2
 	setUseVersion2:function(){
 		translate.useVersion = 'v2';

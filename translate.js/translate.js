77501642429bf25a1950786709aa71495c611599
xnx3 <mail@xnx3.com>
2023-12-25 16:38:33 +0800

    v2.10.2.20231225

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -13,7 +13,7 @@ var translate = {
 	useVersion:'v2',	//当前使用的版本，默认使用v1. 可使用 setUseVersion2(); //来设置使用v2
 	setUseVersion2:function(){
 		translate.useVersion = 'v2';
-		console.log('自 v2.10 之后的版本默认就是使用V2版本， translate.setUseVersion2() 可以不用再加这一行了。当然加了也无所谓，只是加了跟不加是完全一样的。');
+		console.log('提示：自 v2.10 之后的版本默认就是使用V2版本（当前版本为:'+translate.version+'）， translate.setUseVersion2() 可以不用再加这一行了。当然加了也无所谓，只是加了跟不加是完全一样的。');
 	},
 	/*
 	 * 翻译的对象，也就是 new google.translate.TranslateElement(...)
@@ -3610,7 +3610,7 @@ var translate = {
 					xhr.setRequestHeader(index,headers[index]);
 				}
 			}
-			xhr.setRequestHeader('CurrentPage', window.location.href+'');
+			xhr.setRequestHeader('currentpage', window.location.href+'');
 			xhr.send(params);
 			//4.请求状态改变事件
 			xhr.onreadystatechange=function(){

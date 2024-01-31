c6e137d6234284fa89df18aad681e36d56297ec1
xnx3 <mail@xnx3.com>
2024-01-31 11:24:24 +0800

    去掉 api.kefu.leimingyun.com 节点

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -9,8 +9,8 @@ var translate = {
 	/*
 	 * 当前的版本
 	 */
-	version:'2.11.12.20240124',
-	useVersion:'v2',	//当前使用的版本，默认使用v1. 可使用 setUseVersion2(); //来设置使用v2
+	version:'3.0.0.20240131',
+	useVersion:'v2',	//当前使用的版本，默认使用v2. 可使用 setUseVersion2(); //来设置使用v2 ，已废弃，主要是区分是否是v1版本来着，v2跟v3版本是同样的使用方式
 	setUseVersion2:function(){
 		translate.useVersion = 'v2';
 		console.log('提示：自 v2.10 之后的版本默认就是使用V2版本（当前版本为:'+translate.version+'）， translate.setUseVersion2() 可以不用再加这一行了。当然加了也无所谓，只是加了跟不加是完全一样的。');
@@ -3857,7 +3857,7 @@ var translate = {
 			 * v2.8.2 增加数组形态，如 ['https://api.translate.zvo.cn/','xxxxx'] 
 			 */
 			//host:'https://api.translate.zvo.cn/',
-			host:['https://api.translate.zvo.cn/','https://api.kefu.leimingyun.com/', 'https://america.api.translate.zvo.cn/'],
+			host:['https://api.translate.zvo.cn/','https://america.api.translate.zvo.cn/'],
 			//host的备用接口，格式同host，可以填写多个，只不过这里是数组格式。只有当主 host 无法连通时，才会采用备host来提供访问。如果为空也就是 [] 则是不采用备方案。
 			//backupHost:['',''],
 			language:'language.json', //获取支持的语种列表接口

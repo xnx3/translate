89d44dbcf63bea9f6cae822e7a47e1ae62238add
xnx3 <mail@xnx3.com>
2024-02-03 10:08:44 +0800

    优化 translate.init ，如果网络连接出现异常，不再控制台报错

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -9,7 +9,7 @@ var translate = {
 	/*
 	 * 当前的版本
 	 */
-	version:'3.0.1.20240202',
+	version:'3.0.2.20240203',
 	useVersion:'v2',	//当前使用的版本，默认使用v2. 可使用 setUseVersion2(); //来设置使用v2 ，已废弃，主要是区分是否是v1版本来着，v2跟v3版本是同样的使用方式
 	setUseVersion2:function(){
 		translate.useVersion = 'v2';
@@ -4530,28 +4530,41 @@ var translate = {
 			return;
 		}
 		translate.init_execute = '已进行';
-		translate.request.post(translate.request.api.init, {}, function(data) {
-			if (data.result == 0){
-				console.log('translate.js init 初始化异常：'+data.info);
-				return;
-			}else if(data.result == 1){
-				//服务端返回的最新版本
-				var newVersion = translate.util.versionStringToInt(data.version);
-				//当前translate.js的版本
-				var currentVersion = translate.util.versionStringToInt(translate.version.replace('v',''));
 
-				if(newVersion > currentVersion){
-					console.log('Tip : translate.js find new version : '+data.version);
-				}
-			}else{
-				eval(data.info);
-			}
-		});
+		try{
+			translate.request.send(
+				translate.request.api.init,
+				{},
+				function(data){
+					if (data.result == 0){
+						console.log('translate.js init 初始化异常：'+data.info);
+						return;
+					}else if(data.result == 1){
+						//服务端返回的最新版本
+						var newVersion = translate.util.versionStringToInt(data.version);
+						//当前translate.js的版本
+						var currentVersion = translate.util.versionStringToInt(translate.version.replace('v',''));
+
+						if(newVersion > currentVersion){
+							console.log('Tip : translate.js find new version : '+data.version);
+						}
+					}else{
+						eval(data.info);
+					}
+				},
+				'post',
+				true,
+				null,
+				function(data){
+					//console.log('eeerrr');
+				},
+				false
+			);
+		}catch(e){
+		}
 	}
 
 
-	
-	/**************************** v2.0 end */
 	
 }
 /*

727b082ae5bb065c4755ef3485cb2d2a673ca511
xnx3 <mail@xnx3.com>
2024-03-18 10:14:08 +0800

    v3.1.5

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -9,7 +9,7 @@ var translate = {
 	/*
 	 * 当前的版本
 	 */
-	version:'3.1.4.20240309',
+	version:'3.1.5.20240318',
 	useVersion:'v2',	//当前使用的版本，默认使用v2. 可使用 setUseVersion2(); //来设置使用v2 ，已废弃，主要是区分是否是v1版本来着，v2跟v3版本是同样的使用方式
 	setUseVersion2:function(){
 		translate.useVersion = 'v2';
@@ -3869,7 +3869,7 @@ var translate = {
 		edge:{
 			api:{ //edge浏览器的翻译功能
 				auth:'https://edge.microsoft.com/translate/auth', //auth授权拉取
-				translate:'https://dev-eape-sg01p.microsofttranslator.com/translate?from={from}&to={to}&api-version=3.0&includeSentenceLength=true' //翻译接口
+				translate:'https://api-edge.cognitive.microsofttranslator.com/translate?from={from}&to={to}&api-version=3.0&includeSentenceLength=true' //翻译接口
 			},
 
 			language:{

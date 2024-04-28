35a3daa870ba630088995f2f0f89304fc704ccde
xnx3 <mail@xnx3.com>
2024-04-28 11:14:17 +0800

    移除过时的 translate.api

--- translate.api/source/src/main/webapp/js/translate_cover.js
+++
@@ -1,7 +0,0 @@
-translate.requests = new Array();
-translate.request.send = function(url, data, func, method, isAsynchronize, headers, abnormalFunc){
-	translate.requests.push({
-		url:url,
-		data:data
-	});
-}

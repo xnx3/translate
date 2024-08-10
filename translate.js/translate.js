e1e1db6a09c58cc0c6167ec9ffbc043ec444600c
xnx3 <mail@xnx3.com>
2024-08-10 09:53:33 +0800

    Update translate.js

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -9,7 +9,7 @@ var translate = {
 	/*
 	 * 当前的版本
 	 */
-	version:'3.6.2.20240802',
+	version:'3.7.0.20240810',
 	useVersion:'v2',	//当前使用的版本，默认使用v2. 可使用 setUseVersion2(); //来设置使用v2 ，已废弃，主要是区分是否是v1版本来着，v2跟v3版本是同样的使用方式
 	setUseVersion2:function(){
 		translate.useVersion = 'v2';

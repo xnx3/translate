0e018426692156f0e62df147d0b8d39c676817bc
xnx3 <mail@xnx3.com>
2025-01-24 09:21:36 +0800

    Update translate.js

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -12,7 +12,7 @@ var translate = {
 	 * 格式：major.minor.patch.date
 	 */
 	// AUTO_VERSION_START
-	version: '3.12.11.20250122',
+	version: '3.13.0.20250124',
 	// AUTO_VERSION_END
 	/*
 		当前使用的版本，默认使用v2. 可使用 setUseVersion2(); 

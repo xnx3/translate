676c7bedd69d3554d98de1406a03d86ff47eaaef
myFOXforever <3384577917@qq.com>
2025-03-03 16:21:32 +0800

    优化加载动画

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -5970,7 +5970,7 @@ var translate = {
 					  left: 0;
 					  width: 100%;
 					  height: 100%;
-					  background: rgba(255, 255, 255, 0.9); /* 半透明白色遮罩 */
+					  background: rgba(255, 255, 255, 1); /* 半透明白色遮罩 */
 					  z-index: 2;
 					}
 

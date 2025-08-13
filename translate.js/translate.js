ec2a29053e219e196d075b2e3de6b4ce1e7b6e22
xnx3 <mail@xnx3.com>
2025-08-13 14:36:00 +0800

    优化translate.js压缩报错异常

--- translate.js/translate.js
+++ translate.js/translate.js
@@ -8419,13 +8419,14 @@ var nodeuuid = {
 console.log('------ translate.js ------\nTwo lines of js html automatic translation, page without change, no language configuration file, no API Key, SEO friendly! Open warehouse : https://github.com/xnx3/translate \n两行js实现html全自动翻译。 无需改动页面、无语言配置文件、无API Key、对SEO友好！完全开源，代码仓库：https://gitee.com/mail_osc/translate');
 /*js copyright-notice end*/
 
+
 /*js amd-cmd-commonjs start*/
 /*兼容 AMD、CMD、CommonJS 规范 - start*/
 /**
  * 兼容 AMD、CMD、CommonJS 规范
  * node 环境使用：`npm i i18n-jsautotranslate` 安装包
  */
-(function (root, factory) {
+;(function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define([], () => factory());
   } else if (typeof module === 'object' && module.exports) {

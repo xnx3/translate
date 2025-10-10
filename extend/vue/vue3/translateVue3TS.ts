07a8d1fd5396df1a89d1859f2b7730ba896f79db
xnx3 <mail@xnx3.com>
2025-10-10 17:11:14 +0800

    Update translateVue3TS.ts

--- extend/vue/vue3/translateVue3TS.ts
+++ extend/vue/vue3/translateVue3TS.ts
@@ -1,13 +1,6 @@
 import { nextTick } from 'vue';
 import translate from 'i18n-jsautotranslate'
 
-/*
-
-  因为这个文件没什么需要用户单独设置的，只是整体对vue的适配，所以这个文件后续调好了会放到 npm 上，当前因为下面DOM渲染完毕触发的问题没有精准触发，所以暂时先放到这里进行方便优化调试
-
-
-*/
-
 //vue3框架的一些单独设置
 translate.vue3 = {
   /*
@@ -16,8 +9,18 @@ translate.vue3 = {
       false则不会再dom渲染完后自动进行翻译,自然也不会显示 select 选择语言
   */
   isExecute: false, 
+  /*
+    是否在vue入口文件中 调用了 translateJsVueUseModel 插件 app.use(translateJsVueUseModel) 
+    有则是true，没有则是false
+  */
+  isUse: false,
+  /* 
+    用户自定义配置，将在 app.use(translateJsVueUseModel)  时触发这个方法的执行
+  */
+  config: function(app){}
 }
-//如果网页上有 translate.execute() 代码的触发，那么就设置 isExecute 为 true
+
+//如果 translate.vue3.config 中有 translate.execute() 代码的触发，那么就设置 isExecute 为 true ，同时阻止此次的 translate.execute() 执行，因为此次执行是在vue3的初始化阶段，而不是在dom渲染完毕后触发的，所以这里只是进行一个标记，待 真实 DOM渲染完毕后再来触发。
 translate.lifecycle.execute.trigger.push(function(data){
   if(data.executeTriggerNumber === 1){
       translate.vue3.isExecute = true;
@@ -25,16 +28,22 @@ translate.lifecycle.execute.trigger.push(function(data){
       return false;  
   }
 });
-translate.time.log('设置vue3初始化配置 - translate.vue3');
 
 //将 translate 参数挂载到 window 上，方便在全局调用
 if(typeof(window.translate) === 'undefined'){
   window.translate = translate;
 }
-translate.time.log('将 translate 参数挂载到 window 上，方便在全局调用');
+translate.time.log('将 translate 参数挂载到 window 上，方便在全局调用及调试');
+
 
 const translateJsVueUseModel = {
   install(app) {
+    translate.time.log(' app.use(translateJsVueUseModel) ');
+    translate.vue3.isUse = true;
+
+    //触发自定义配置
+    translate.vue3.config(app);
+
     // 直接监听应用挂载完成
     const originalMount = app.mount;
     app.mount = function(...args) {
@@ -53,22 +62,13 @@ const translateJsVueUseModel = {
           
           */
 
-
           if(translate.vue3.isExecute){
             translate.time.log('组件渲染完成，触发 translate.execute();');
 
             //对vue3的某些第三方组件进行容错处理
             translate.faultTolerance.documentCreateTextNode.use(); //对VUE的某些组件频繁渲染dom进行容错
             translate.time.log('对vue3的某些第三方组件进行容错处理 - translate.faultTolerance.documentCreateTextNode.use();');
-
-            console.log(translate.getDocuments());
             translate.execute();
-            setTimeout(() => {
-              translate.execute();
-            }, 100);
-            setTimeout(() => {
-              translate.execute();
-            }, 2000);
           }else{
             translate.time.log('组件渲染完成，但未发现translate.execute();存在，不进行翻译');
           }

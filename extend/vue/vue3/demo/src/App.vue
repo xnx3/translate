540b42f47dcbb4e8699affeb0bf84d9ae855b105
xnx3 <mail@xnx3.com>
2025-10-21 18:00:38 +0800

    增加 VUE3 的 Demo

---
+++ extend/vue/vue3/demo/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Welcome from './components/Welcome.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Welcome/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
